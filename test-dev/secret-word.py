1b825f7f6c48cc79786dc577bc3141b3ed364ce4
PrabhatJadhav <prabhatjadhav.exe@gmail.com>
2024-08-21 20:04:26 +0530

    secret word game done

--- test-dev/secret-word.py
+++ test-dev/secret-word.py
@@ -4,17 +4,17 @@ guess_count=0
 guess_limit=3
 is_guess_correct=False
 
-while not(is_guess_correct) :
-    if not(is_guess_correct) and guess_count < guess_limit:
+while not(is_guess_correct) and guess_count < guess_limit :
+    if guess_count < guess_limit:
         user_input=input("Guess the word: ")
         guess_count +=1
-        is_guess_correct=False
-    else:
-        is_guess_correct=True
-        break
+        if user_input == secret_word:
+            is_guess_correct=True
+            break
+    
 
-if (guess_count < guess_limit) and is_guess_correct:
-    print("Correct Guess ==> " + user_input)
+if (guess_count <= guess_limit) and is_guess_correct:
+    print("Correct Guess, secret word was ==> " + user_input)
     print("Guessed In ==> " , guess_count , " tries")
 else:
     print("Guess Limit reached, you had",guess_limit,"guesses, YOU LOSE!!!")
