9ab5e79776c9f6bc7d93e35e0089527bddb2846a
PrabhatJadhav <prabhatjadhav.exe@gmail.com>
2024-09-02 20:40:10 +0530

    two sum

--- dsa/two-sum.py
+++ dsa/two-sum.py
@@ -2,3 +2,19 @@ array =[2,7,11,6]
 target=13
 
         
+def twoSum(nums, target):
+    numbers_dictionary = dict()
+    
+    for index,a_number in enumerate(nums):
+        target_number = abs( target - a_number)
+        
+        
+        if target_number in numbers_dictionary:
+            return [index,numbers_dictionary[target_number]]
+        
+        numbers_dictionary[a_number]=index
+        
+        
+        
+print("Answer ==>",twoSum(array,target))
+
